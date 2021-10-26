0881f7f13bc57d1630e1a7501cc6b385aadc4afa
youshamahmood <youshamahmood96@gmail.com>
2021-10-26 21:30:32 +0600

    folder structure changed

--- Leetcode/LinkedList/L1290.java
+++ Leetcode/LinkedList/L1290.java
@@ -2,12 +2,22 @@ package Leetcode.LinkedList;
 
 import java.util.LinkedList;
 
-public class ListNode {
+class ListNode {
     int val;
     ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 
 public class L1290 {
     public int getDecimalValue(ListNode head) {
