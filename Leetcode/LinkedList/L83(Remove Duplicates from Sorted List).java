92d9595efdb434e8ec999cc70f2e357277c6193f
youshamahmood <youshamahmood96@gmail.com>
2021-10-30 02:42:07 +0600

    Update L83(Remove Duplicates from Sorted List).java

--- Leetcode/LinkedList/L83(Remove Duplicates from Sorted List).java
+++ Leetcode/LinkedList/L83(Remove Duplicates from Sorted List).java
@@ -10,40 +10,19 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if(head == null) return null;
-        HashSet<Integer> set = new HashSet<>();
-        ListNode curr = head;
-        while(curr!=null){
-            if(!set.contains(curr.val)){
-                set.add(curr.val);
+        if(head == null || head.next == null) return head;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast!=null){
+            if(slow.val == fast.val){
+                slow.next = fast.next;
+                fast = fast.next;
             }
-            curr = curr.next;
+            else{
+                slow = slow.next;
+                fast = fast.next;
+            }a
         }
-        int n = set.size();
-        Integer arr[] = new Integer[n];
-        int i = 0;
-        for (Integer x : set)
-            arr[i++] = x;
-        Arrays.sort(arr);
-        ListNode res = new ListNode();
-        res.val = arr[0];
-        curr = res;
-        res.next = new ListNode();
-        res = res.next;
-        for(int j = 1; j<arr.length;j++){
-            res.val = arr[j];
-            res.next = new ListNode();
-            res = res.next;
-        }
-        res.next = null;
-        res = curr;
-        while(res.next!=null){
-            if(res.next.next == null){
-                res.next = null;
-                break;
-            }
-            res = res.next;
-        }
-        return curr;
+        return head;
     }
 }
