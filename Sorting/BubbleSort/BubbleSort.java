a90460386a1477d58951ab446a13b43c3824094a
youshamahmood <youshamahmood96@gmail.com>
2021-11-01 18:44:24 +0600

    Update BubbleSort.java

--- Sorting/BubbleSort/BubbleSort.java
+++ Sorting/BubbleSort/BubbleSort.java
@@ -1,7 +1,3 @@
-// Start looping the end of the array, with a pointer named j. This will have one decrement per loop. It will stop looping once it reaches the beginning of the array.
-// Start an inner loop with another pointer named jk, it will have one increment per loop. It will stop looping once it reaches the poiter j.
-// If arr[j+1]>arr[j], swap.
-// return the sorted array.
 static int[] bubbleSort (int[] arr){
   for(int i=arr.length-1;i>=0;i--){
     for(int j=0;j<i;j++){
@@ -15,8 +11,7 @@ static int[] bubbleSort (int[] arr){
   return arr;
 }
 
-// Optimization
-// We dont need to keep on looping if one chunk of the array is already sorted. So we can set a flag to keep track of that.
+// Optimized
 
  static int[] bubbleSort (int[] arr){
     boolean swapped = false;
@@ -33,6 +28,4 @@ static int[] bubbleSort (int[] arr){
     }
     return arr;
   }
-// BigO
-// worst case --> 0(n^2) (If the data is not sorted at all)
-// best case --> 0(n) (If the data is nearly sorted/already sorted, using the second code, which is optimized)
+
