f2deddbf51230bceb66bde4892f64ed6d78af719
youshamahmood <youshamahmood96@gmail.com>
2021-11-01 18:21:37 +0600

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -14,3 +14,22 @@ static int[] bubbleSort (int[] arr){
   }
   return arr;
 }
+
+// Optimization
+// We dont need to keep on looping if one chunk of the array is already sorted. So we can set a flag to keep track of that.
+
+ static int[] bubbleSort (int[] arr){
+    boolean swapped = false;
+    for(int i=arr.length-1;i>=0;i--){
+      for(int j=0;j<i;j++){
+        if(arr[j]>arr[j+1]){
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+          swapped = true;
+        }
+      }
+      if(swapped) break;
+    }
+    return arr;
+  }
