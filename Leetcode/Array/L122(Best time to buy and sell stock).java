e004e33477f948ddfd835ef5fe17f2c85118c32b
youshamahmood <youshamahmood96@gmail.com>
2021-11-23 20:35:17 +0600

    Update L122(Best time to buy and sell stock).java

--- Leetcode/Array/L122(Best time to buy and sell stock).java
+++ Leetcode/Array/L122(Best time to buy and sell stock).java
@@ -1,19 +1,21 @@
-class Solution {
-    public int maxProfit(int[] prices) {
-        int i = 0;
-        int j = 1;
-        int d = 0;
-        while(j<prices.length){
-            if(prices[i]>prices[j]){
-                i++;
-                j++;
-            }
-            else{
-                d = d + (prices[j]-prices[i]);
-                i++;
-                j++;
-            }
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let max = 0
+    let i = 0
+    let j = 1
+    let curr
+    while(j<prices.length){
+        if(prices[j]>prices[i]){
+            curr = prices[j] - prices[i]
+            max = Math.max(curr,max)
         }
-        return d;
+        else{
+            i = j
+        }
+        j++
     }
-}
+    return max
+};
